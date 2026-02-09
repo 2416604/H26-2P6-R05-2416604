76783b295110f708c6870e4738300d572a7edf44
2416604 <2416604@cegepmontpetit.ca>
2026-02-09 13:12:39 -0500

    Projet initial

---
+++ Exercice1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Exercice1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Écriture des trois fichiers csv...");
+            List<string[]> texteDepart = new List<string[]>()
+            {
+                new string[3] {"Prénom", "Nom", "Note" },
+                new string[3] {"Jamil", "Gammoudi", "95" },
+                new string[3] {"David", "Gagné-Leroux", "90" },
+                new string[3] {"Jimmy", "Beaubien", "88" },
+                new string[3] {"Philippe", "Martel", "32" },
+            };
+        }
+    }
+}
---
+++ Exercice2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Exercice2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Animal[] animaux =
+            //{
+            //    new Animal("Ghost", (Animal.TypeAnimal)99),
+            //    new Animal("Mia", Animal.TypeAnimal.Chat),
+            //    new Animal("Snoopy", Animal.TypeAnimal.Chien),
+            //    new Animal("Simba", Animal.TypeAnimal.Lion),
+            //    new Animal("Gary", Animal.TypeAnimal.Serpent)
+            //};
+
+        }    
+    }
+}
