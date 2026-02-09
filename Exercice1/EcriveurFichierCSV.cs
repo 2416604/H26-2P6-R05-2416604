2f06b0ea6c8d5f0f99aea88f7344870c46bb0788
2416604 <2416604@cegepmontpetit.ca>
2026-02-09 14:59:25 -0500

    Sauv1

---
+++ Exercice1/EcriveurFichierCSV.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace Exercice1
+{
+    internal class EcriveurFichierCSV
+    {
+        // Constantes
+        public const string CHEMIN_PAR_DEFAUT = @"C:\EspaceLabo";
+        public const string NOM_FICHIER_DEFAUT = "monFichier.csv";
+
+
+        public enum TypeDelimiteur { Virgule, PointVirgule, LigneVerticale }
+        // Champs 
+        private string cheminAcces;
+        private string nomFichier;
+        private TypeDelimiteur _typeDelimiteur;
+
+        //Propriétés
+        public string CheminAccess { get; private set; }
+        public string NomFichier { get; private set; }
+        private TypeDelimiteur DelimiteurChoisi { get; set; }
+
+        //Méthodes
+        public void EcrireLignr(string[] colonnes) { }
+
+        public EcriveurFichierCSV(string cheminAcces, string nomFichier, TypeDelimiteur delimiteur){ }
+
+    }
+
+    
+}
--- Exercice1/Program.cs
+++ Exercice1/Program.cs
@@ -1,4 +1,9 @@
-﻿namespace Exercice1
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+namespace Exercice1
 {
     internal class Program
     {
@@ -7,11 +12,21 @@
             Console.WriteLine("Écriture des trois fichiers csv...");
             List<string[]> texteDepart = new List<string[]>()
             {
+
                 new string[3] {"Prénom", "Nom", "Note" },
                 new string[3] {"Jamil", "Gammoudi", "95" },
                 new string[3] {"David", "Gagné-Leroux", "90" },
                 new string[3] {"Jimmy", "Beaubien", "88" },
                 new string[3] {"Philippe", "Martel", "32" },
+
+
+
+
+
+
+
+
+
             };
         }
     }
